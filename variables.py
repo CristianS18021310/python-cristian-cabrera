eb6ca6e73e3d996f089ea21f097beac10c46fe41
CristianS18021310 <zS18021310@estudiantes.uv.mx>
2021-04-16 02:53:39 +0000

    add variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+#variables.py
+#Cristian Cabrera
+year = 2020
+sentence = "My name is Eduardo"
+sarah, bob, mike = 16, 20, 25
+name, age = "Eduardo", 21
+cris = eduar = 21
+PI = 3.1416
+e= 2.71
+
+# display values
+print "year = ", year
+print sentence
+print "\n sarah : ", sarah, "\n bob =", bob, "\n mike =", mike
+print "\n\t name =", name
+print "\n\t age =", age
+print mary, fer
+print PI
+print e
