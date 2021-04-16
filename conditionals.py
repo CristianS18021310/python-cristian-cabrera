090435422f80e027c9676a267a03f990961f062e
CristianS18021310 <zS18021310@estudiantes.uv.mx>
2021-04-16 04:06:09 +0000

    add conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+#if 
+nucleotide = raw_input("Input nucleotic A, C, T, G :")
+print nucleotide
+
+if (nucleotide == "A"):
+  print ("ADENINA")
+elif (nucleotide == "c"):
+  print ("CITOSINA")
+elif (nucleotide == "T"):
+  print ("TIMINA")
+elif (nucleotide == "G"):
+  print ("GUANINA")
+else:
+  print ("ERROR in entrada" ) 
